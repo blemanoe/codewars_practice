3c68bb3331bdba9270cd08f2c8fe737a13b0a7f2
Kelvin Okornoe <kelvin.okornoe@gmail.com>
2024-11-18 17:59:11 +0000

    init commit

---
+++ Codewars/find_parity_outlier.py
@@ -0,0 +1,23 @@
+def find_outlier(integers):
+    if integers[0]%2 == 0 and integers[1]%2 == 0:
+        for x in range(len(integers)):
+            if integers[x]%2 == 1:
+                return integers[x]
+
+    if integers[0]%2 == 1 and integers[1]%2 == 1:
+        for x in range(len(integers)):
+            if integers[x] % 2 == 0:
+                return integers[x]
+
+    if integers[0]%2 != integers[1]%2:
+        if integers[0]%2 == 1 and integers[2]%2 == 1:
+            for x in range(len(integers)):
+                if integers[x] % 2 == 0:
+                    return integers[x]
+        else:
+            for x in range(len(integers)):
+                if integers[x] % 2 == 1:
+                    return integers[x]
+
+if __name__ == "__main__":
+    print(find_outlier([160, 3, 1719, 19, 11, 13, 21]))
